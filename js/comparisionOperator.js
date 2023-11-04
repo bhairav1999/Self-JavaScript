b83b8f81b6c6470f55b08999126342625b004591
bhairav1999 <bhairavkarande1999@gmail.com>
2023-11-04 14:01:17 +0530

    comparistion operator

---
+++ js/comparisionOperator.js
@@ -0,0 +1,14 @@
+console.log(null === 0) // false
+console.log(undefined == 0)  // false
+console.log(undefined <= 0)  // false
+console.log(null <= 0)  // true
+
+console.log(Symbol("123"))
+
+
+const myfunction = function(){
+    console.log("my function")
+}
+myfunction()
+
+console.log(typeof myfunction)
